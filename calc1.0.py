93395a8386bbac3e32e3a2b75ed0028a2e3c5f77
celitos <93286134+celitos@users.noreply.github.com>
2022-07-13 21:44:13 -0300

    Create calc1.0.py

---
+++ calc1.0.py
@@ -0,0 +1,27 @@
+while True:
+  
+  print()
+  num_1 = input('Set first number: ')
+  num_2 = input('Set another number: ')
+  operador = input('Set operator: ')
+  
+  if not num_1.isnumeric() or not num_2.isnumeric():
+    print('You need to type a number.')
+    continue
+  
+  num_1 = int(num_1)
+  num_2 = int(num_2)
+  
+  # + - * /
+  if operador == '+':
+    print(num_1 + num_2)
+  elif operador == '-':
+    print(num_1 - num_2)
+  elif operador == '*':
+    print(num_1 * num_2)
+  elif operador == '/':
+    print(num_1 / num_2)
+  else:
+    print('Not valid')
+    
+ ###end of code at this moment updates sooner
